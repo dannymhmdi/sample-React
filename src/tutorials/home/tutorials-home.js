5169330b8e2d6ac2d0eed6d0b9df7909f4db460f
dannymhmdi <dannymohammadi1377@gmail.com>
2023-07-06 23:52:15 +0330

    tutorials-home.js

--- src/tutorials/home/tutorials-home.js
+++ src/tutorials/home/tutorials-home.js
@@ -3,14 +3,12 @@
 import BasePage from "../../components/base-page/base-page";
 
 const TutorialsHome = () => {
-  const handleAlert = () => {
-    alert('test is done')
-  } 
+  
   return (
     <BasePage fluid={true} title={'tutorials-home'}>
     <div>
       <h2>React Tutorial</h2>
-      {/* <button onClick={handleAlert}>Event Test</button> */}
+   
       <Event/>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, cumque
